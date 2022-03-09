b2c0c7644cf29731964b89ca1c15d3c32f41dbbd
Erica <ericamisiorski@gmail.com>
2022-03-09 17:05:50 -0500

    fixing

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/budget';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/myFirstDatabase';
 
 
 app.use(logger("dev"));
